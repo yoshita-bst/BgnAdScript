6f393d7cc44e69edbbab3debe39725ace61fe9f8
Yoshita Arora <yoshita@Yoshitas-MacBook-Pro.local>
2020-11-23 17:49:42 +0530

    added all modifications to my github gh-pages link too

--- as_script.js
+++ as_script.js
@@ -10,6 +10,13 @@
    Fetches window.location.search
    and returns an object of corresponding key value pairs
 */
+
+let version = "1.1.0";
+
+console.log("version:", version);
+
+getUrlParams();
+
 function getUrlParams() {
     window.urlParams = {};
     return window.location.search.slice(1).split("&").forEach(it => { window.urlParams[it.split("=")[0]] = it.split("=")[1]})
@@ -19,12 +26,12 @@ function getUrlParams() {
     Fetches all the ads through and API call
 */ 
 function getAds () {
-    let adDiv = document.getElementsByClassName("adsbybgn");
-    window.urlParams.logs && console.log('inside GET Ads');
+    let adDiv = document.getElementsByClassName("asbybgn");
+    window.urlParams === "true" && console.log('inside GET Ads');
     for(let i = 0; i < adDiv.length; i++){
         let w = adDiv[i].style.width.split('px')[0];
         let h = adDiv[i].style.height.split('px')[0];
-        let sid = adDiv[i].getAttribute("data-bgn-ad-slot");
+        let sid = adDiv[i].getAttribute("data-bgn-as-slot");
         let ed = {
             w,
             h,
@@ -32,12 +39,12 @@ function getAds () {
         }
         let params = getUrlFromParams();
         params = `${params}&ed=${JSON.stringify(ed)}`;
-        window.urlParams.logs && console.log('Ads call PARAMS', params);
-        fetch(`https://bgn.gg/as/c?${params}`, {
+        window.urlParams.bgnLogs === "true" && console.log('Ads call PARAMS', params);
+        fetch(`https://bgn-1-dot-bluestacks-cloud-qa.appspot.com/as/c?${params}`, {
             method: 'GET',
             credentials: 'include',
         }).then((res)=>(res.json())).then((data)=>{
-            window.urlParams.logs && console.log('GET Ads SUCCESS:', data);
+            window.urlParams.bgnLogs === "true" && console.log('GET Ads SUCCESS:', data);
             loadAd(data, adDiv[i]);
             params = getUrlFromParams();
             ed.p = data.p;
@@ -51,56 +58,232 @@ function getAds () {
                 ed.c = data.c;
                 ed.i = data.i;
                 params = `${params}&ed=${JSON.stringify(ed)}`;
-                window.urlParams.logs && console.log('Ad click PARAMS:', params);
+                window.urlParams.bgnLogs === "true" && console.log('Ad click PARAMS:', params);
                 window.open(data.c);
                 sendImpression('ac', params);
             }
         }).catch(err=>{
-            window.urlParams.logs && console.log('GGET Ads FAILURE:', err);
+            window.urlParams === "true" && console.log('GGET Ads FAILURE:', err);
         })
     }
+}
+
+function clickAdDiv (adDiv, data) {
+    let w = adDiv.style.width.split('px')[0];
+    let h = adDiv.style.height.split('px')[0];
+    let sid = adDiv.getAttribute("data-bgn-as-slot");
+    let ed = {
+        w,
+        h,
+        sid: sid,
+    }
+    params = getUrlFromParams();
+    ed.p = data.p;
+    ed.u = data.u;
+    ed.c = data.c;
+    ed.i = data.i;
+    params = `${params}&ed=${JSON.stringify(ed)}`;
+    window.urlParams.bgnLogs === "true" && console.log('Ad click PARAMS:', params);
+    window.open(data.c);
+    sendImpression('ac', params);
 }
 
 
 /* Adds styling to the 'Ad' text of the ad unit */
 function styleAdButton (adButton) {
-    adButton.style.background = "#eee";
-    adButton.style.color = "blue";
-    adButton.style.width = "15px";
-    adButton.style.height = "15px";
-    adButton.style.fontSize = "10px";
+    adButton.style.background = "#EEBA00";
+    adButton.style.color = "#171717";
+    adButton.style.width = "20px";
+    adButton.style.height = "20px";
+    adButton.style.fontSize = "12px";
     adButton.style.position = "relative";
     adButton.style.display = "flex";
     adButton.style.alignItems = "center";
     adButton.style.justifyContent = "center";
+    adButton.style.cursor = "default";
 }
 
 
 /* Adds styling to the 'i' image of the add unit */
 function styleiImage(iImage) {
-    iImage.style.width = "15px";
-    iImage.style.height = "15px";
+    iImage.style.background = "black";
+    iImage.style.width = "20px";
+    iImage.style.height = "20px";
     iImage.style.position = "absolute";
-    iImage.style.top = "0";
-    iImage.style.right = "0";
-    iImage.hover
+    iImage.style.top = "3px";
+    iImage.style.right = "28px";
+    iImage.style.cursor = "default";
+}
+
+function stylexImage(iImage) {
+    iImage.style.background = "black";
+    iImage.style.width = "20px";
+    iImage.style.height = "20px";
+    iImage.style.position = "absolute";
+    iImage.style.top = "3px";
+    iImage.style.right = "4px";
+    iImage.style.cursor = "pointer";
 }
 
 /* adds styling to the div that appears on hover of the 'i' image of ad unit */ 
 function styleHoverDiv(hoverDiv) {
-    hoverDiv.style.background = "#eee";
-    hoverDiv.style.color = "blue";
+    hoverDiv.style.background = "white";
+    hoverDiv.style.color = "#0082E5";
     hoverDiv.style.width = "65px";
-    hoverDiv.style.height = "15px";
-    hoverDiv.style.fontSize = "10px";
+    hoverDiv.style.fontSize = "12px";
     hoverDiv.style.position = "absolute";
     hoverDiv.style.display = "flex";
     hoverDiv.style.alignItems = "center";
     hoverDiv.style.justifyContent = "center";
-    hoverDiv.style.top = "0px";
-    hoverDiv.style.right = "15px";
+    hoverDiv.style.top = "3px";
+    hoverDiv.style.right = "50px";
     hoverDiv.style.visibility = "hidden";
+    hoverDiv.style.padding = "3px 7px";
+}
 
+function addAdButton(adDiv){
+    let adButton = document.createElement('div');
+    styleAdButton(adButton);
+    let textNode = document.createTextNode('Ad')
+    adButton.appendChild(textNode);
+    adButton.onclick = function(e){
+        e.stopPropagation();
+
+    }
+    adDiv.appendChild(adButton);
+    return adButton;
+}
+
+function addiIconAndHoverDiv(adDiv){
+    let iImage = document.createElement('img');
+    styleiImage(iImage);
+    iImage.src = "https://cdn-bgp.bluestacks.com/bgp/BGP_Snippet_Code/icons/i_icon.png";
+    adDiv.appendChild(iImage);
+    let hoverDiv = document.createElement('div');
+    let uniqueId = getRandomNumber();
+    hoverDiv.id = uniqueId;
+    styleHoverDiv(hoverDiv);
+    hoverDiv.innerHTML = "Ads by BGN";
+    iImage.onclick = function(e){
+        e.stopPropagation();
+    }
+    iImage.onmouseover = function(e){
+        hoverDiv.style.visibility = 'visible';
+    }
+    iImage.onmouseout = function(e){
+        hoverDiv.style.visibility = 'hidden';
+    }
+    adDiv.appendChild(hoverDiv);
+    return iImage
+}
+
+function styleContainer(container){
+    container.style.display = "flex";
+    container.style.alignItems = "center";
+    container.style.border = "1px solid #DDDDDD";
+    container.style.justifyContent = "center";
+    container.style.flexDirection = "column";
+    container.style.fontSize = "24px";
+    container.style.color = "#4E4E4E";
+    container.style.width = "300px";
+    container.style.height = "250px";
+}
+function styleHeadingText(heading){
+    heading.style.display = "flex";
+    heading.style.alignItems = "center";
+    heading.style.justifyContent = "center";
+}
+
+function styleStopAdButton(button){
+    button.style.background = "#0082E5";
+    button.style.borderRadius = "5px";
+    button.style.color = "white";
+    button.style.cursor = "pointer";
+    button.style.display = "flex";
+    button.style.alignItems = "center";
+    button.style.justifyContent = "center";
+    button.style.padding = "8px 15px";
+    button.style.fontSize = "16px";
+    button.style.marginTop = "14px";
+}
+
+function styleBgnImage(bgnImage){
+    bgnImage.style.marginLeft = "9px";
+}
+
+function addHeadingText(adDiv){
+    let container = document.createElement('div');
+    styleContainer(container);
+    let heading = document.createElement('div');
+    styleHeadingText(heading);
+    let textNode = document.createTextNode('Ads by')
+    heading.appendChild(textNode);
+    let bgnImage = document.createElement('img');
+    styleBgnImage(bgnImage);
+    bgnImage.src = "http://cdn-bgp.bluestacks.com/bgp/BGP_Snippet_Code/icons/bgn.png";
+    heading.appendChild(bgnImage);
+    container.appendChild(heading);
+    addStopButton(container);
+    adDiv.appendChild(container);
+    return container;
+
+}
+
+function addStopButton(headingDiv){
+    let button = document.createElement('div');
+    styleStopAdButton(button);
+    let textNode = document.createTextNode('Stop seing this Ad')
+    button.appendChild(textNode);
+    headingDiv.appendChild(button);
+}
+
+function styleBackButton(button) {
+    button.style.width = "22px";
+    button.style.height = "18px";
+    button.style.position = "absolute";
+    button.style.top = "12px";
+    button.style.left = "13px";
+}
+
+function addBackButton(adDiv, iImage, adButton, xImage, container, adData){
+    let backButton = document.createElement('img');
+    styleBackButton(backButton);
+    backButton.src = "http://cdn-bgp.bluestacks.com/bgp/BGP_Snippet_Code/icons/Back.png";
+    backButton.onclick = function(e) {
+        backButton.style.display = "none";
+        iImage.style.visibility = "visible";
+        adButton.style.display = "flex";
+        xImage.style.visibility = "visible";
+        container.style.display = "none";
+        adDiv.style.backgroundImage = `url('${adData.u}')`;
+        adDiv.onclick = function(e) {
+            clickAdDiv(adDiv, adData);
+        }
+        e.stopPropagation();
+        
+    }
+    adDiv.appendChild(backButton);
+}
+
+function addCrossIcon(adDiv, iImage, adButton, adData){
+    let xImage = document.createElement('img');
+    stylexImage(xImage);
+    xImage.src = "https://cdn-bgp.bluestacks.com/bgp/BGP_Snippet_Code/icons/cross_icon.png";
+    xImage.onclick = function(e){
+        e.stopPropagation();
+        adDiv.style.background = "rgba(255, 255, 255, 0.5)";
+        // adDiv.style.border = "1px solid #DDDDDD";
+        iImage.style.visibility = "hidden";
+        adButton.style.display = "none";
+        xImage.style.visibility = "hidden";
+        adDiv.onclick = function(e) {
+            return;
+        }
+        let container = addHeadingText(adDiv);
+        addBackButton(adDiv, iImage, adButton, xImage, container, adData);
+
+   }
+    adDiv.appendChild(xImage);
 }
 
 /* 
@@ -112,7 +295,7 @@ function loadAd (data, adDiv) {
     s.src = data.u;
     let w = adDiv.style.width.split('px')[0];
     let h = adDiv.style.height.split('px')[0];
-    let sid = adDiv.getAttribute("data-bgn-ad-slot");
+    let sid = adDiv.getAttribute("data-bgn-as-slot");
     let ed = {
         w,
         h,
@@ -124,33 +307,16 @@ function loadAd (data, adDiv) {
     let params = getUrlFromParams();
     params = `${params}&ed=${JSON.stringify(ed)}`;
     s.onerror = function(){
-        adDiv.style.backgroundImage = "url('https://semantic-ui.com/images/wireframe/square-image.png')";
+        // adDiv.style.backgroundImage = "url('https://semantic-ui.com/images/wireframe/square-image.png')";
         sendImpression('ae', params);
     }
     s.onload = function(){
         adDiv.style.backgroundImage = `url('${data.u}')`;
         adDiv.style.position = "relative";
-        let adButton = document.createElement('div');
-        adButton.className = 'adb';
-        styleAdButton(adButton);
-        adButton.innerHTML = "Ad";
-        adDiv.appendChild(adButton);
-        let iImage = document.createElement('img');
-        styleiImage(iImage);
-        iImage.src = "https://tpc.googlesyndication.com/pagead/images/abg/icon.png";
-        adDiv.appendChild(iImage);
-        let hoverDiv = document.createElement('div');
-        let uniqueId = getRandomNumber();
-        hoverDiv.id = uniqueId;
-        styleHoverDiv(hoverDiv);
-        hoverDiv.innerHTML = "Ads by BGN";
-        iImage.onmouseover = function(e){
-            hoverDiv.style.visibility = 'visible';
-        }
-        iImage.onmouseout = function(e){
-            hoverDiv.style.visibility = 'hidden';
-        }
-        adDiv.appendChild(hoverDiv);
+        adDiv.style.textDecoration = "none";
+        let adButton = addAdButton(adDiv);
+        let iImage = addiIconAndHoverDiv(adDiv);
+        addCrossIcon(adDiv, iImage, adButton, data);
     }
 }
 
@@ -262,8 +428,8 @@ function getRandomNumber() {
     necessary to be sent as params in stats api calls 
 */
 function getParams() {
-    let adDiv = document.getElementsByClassName("adsbybgn");
-    let id = adDiv[0].getAttribute('data-bgn-ad-client');
+    let adDiv = document.getElementsByClassName("asbybgn");
+    let id = adDiv[0].getAttribute('data-bgn-as-client');
     let cn = (new URL(window.location.href)).searchParams.get('utm_campaign');
     let cs = (new URL(window.location.href)).searchParams.get('utm_source');
     let ct = (new URL(window.location.href)).searchParams.get('utm_term');
@@ -306,27 +472,32 @@ function getParams() {
 
 /* stats api call */
 function sendImpression (type, params) {
-    window.urlParams.logs && console.log('Sending Impression');
+    window.urlParams === "true" && console.log('Sending Impression');
     if(!params) {
         params = getUrlFromParams();
     }
     params = params + `&ev=${type}`;
-    fetch(`https://bgn.gg/as/i?${params}`, {
+    fetch(`https://bgn-1-dot-bluestacks-cloud-qa.appspot.com/as/i?${params}`, {
         method: 'GET',
         credentials: 'include'
     }).then((res)=>{
-        window.urlParams.logs && console.log('Impression SUCCESS', type);
+        window.urlParams === "true" && console.log('Impression SUCCESS', type);
     }).catch(err => {
-        window.urlParams.logs && console.log('Impression ERROR', err);
+        window.urlParams.bgnLogs === "true" && console.log('Impression ERROR', err);
     })
 }
 
-/* fires on DOM load */
-document.addEventListener("DOMContentLoaded", function(){
-    getUrlParams();
-    window.urlParams.logs && console.log('DOM content loaded');
+/* fires on DOM load or ready state */
+if( document.readyState !== 'loading' ) {
+    window.urlParams.bgnLogs === "true" && console.log('Ready state already');
     sendImpression('pv');
     getAds();
-});
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        window.urlParams.bgnLogs === "true" && console.log('DOM content loaded');
+        sendImpression('pv');
+        getAds();
+    });
+}
 
 
